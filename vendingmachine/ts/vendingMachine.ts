ec6ad64f57569d20c3a39d5ed3582667de91c377
Benjamin Tse <benjamin.tse@fred.com.au>
2017-08-18 16:45:25 +1000

    Selecting and paying

--- vendingmachine/ts/vendingMachine.ts
+++ vendingmachine/ts/vendingMachine.ts
@@ -18,9 +18,12 @@ class Cell {
 
 class VendingMachine {
     private paid = ko.observable(0);
+    selectedCell = ko.observable(new Cell(new CocaCola()));
     cells = ko.observableArray([]);
     acceptedCoins: Quarter[] = [new Quarter()];
     
+    canPay = ko.pureComputed(() => this.paid() - this.selectedCell().product.price >= 0);
+
     set size(givenSize: VendingMachineSize) {
         this.cells([]);
         
@@ -31,7 +34,25 @@ class VendingMachine {
     }
     
     acceptCoin = (coin: Quarter): void => {
+    select = (cell: Cell): void => {
+        cell.sold(false);
+        this.selectedCell(cell);
+    }
+
         let oldTotal = this.paid();
         this.paid(oldTotal + coin.Value);
+    }
+
+    pay = (): void => {
+        if (this.selectedCell().stock() < 1) {
+            alert("I'm sorry, we're out of them!");
+            return;
+        }
+
+        let currentPaid = this.paid();
+        this.paid(Math.round(((currentPaid - this.selectedCell().product.price) * 100)) / 100);
+        let currentStock = this.selectedCell().stock();
+        this.selectedCell().stock(currentStock - 1);
+        this.selectedCell().sold(true);
     }
 }
